577e2d400f443aafa8afb237543f22b227b845e6
fallowShades <fallowshades@gmail.com>
2023-10-06 16:57:49 +0200

    Assets/wrapper Landing
    https://hamoria.atlassian.net/browse/HM-14

--- hamoriaClient/src/pages/Landing.jsx
+++ hamoriaClient/src/pages/Landing.jsx
@@ -2,10 +2,11 @@ import main from '../assets/images/main.svg'
 import { Link } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
 
 const Landing = () => {
   return (
-    <StyledWrapper>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobify" className="logo" />
       </nav>
@@ -30,56 +31,8 @@ const Landing = () => {
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
-    </StyledWrapper>
+    </Wrapper>
   )
 }
-
-const StyledWrapper = styled.section`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-    margin-bottom: 1.5rem;
-  }
-  p {
-    line-height: 2;
-    color: var(--text-secondary-color);
-    margin-bottom: 1.5rem;
-    max-width: 35em;
-  }
-  .register-link {
-    margin-right: 1rem;
-  }
-  .main-img {
-    display: none;
-  }
-  .btn {
-    padding: 0.75rem 1rem;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 400px;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`
 
 export default Landing
