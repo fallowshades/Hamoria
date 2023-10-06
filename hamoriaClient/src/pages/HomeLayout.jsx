8d4bb606823814a6faad1e130641b68c58cef585
fallowShades <fallowshades@gmail.com>
2023-10-06 16:10:05 +0200

    Nested routes
    https://hamoria.atlassian.net/browse/HM-12

--- hamoriaClient/src/pages/HomeLayout.jsx
+++ hamoriaClient/src/pages/HomeLayout.jsx
@@ -1,4 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
 const HomeLayout = () => {
-  return <div>HomeLayout</div>
+  return (
+    <>
+      {/* add things like Navbar */}
+      {/* <h1>home layout</h1> */}
+      <Outlet />
+    </>
+  )
 }
 export default HomeLayout
