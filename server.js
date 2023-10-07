25224199cbd401ad9c2e76ae95b12e8dba0cfe84
fallowShades <fallowshades@gmail.com>
2023-10-07 17:00:25 +0200

    ctrl/router and register (create)
    https://hamoria.atlassian.net/browse/HM-35

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import mongoose from 'mongoose'
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
 
 import { body, validationResult } from 'express-validator'
+
+import authRouter from './routes/authRouter.js'
+
 dotenv.config()
 
 const app = express()
@@ -16,23 +19,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.post(
-  '/api/v1/test',
-  [body('name').notEmpty().withMessage('name is required')],
-  (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map((error) => error.msg)
-      return res.status(400).json({ errors: errorMessages })
-    }
-    next()
-  },
-  (req, res) => {
-    const { name } = req.body
-    res.json({ msg: `hello ${name}` })
-  }
-)
 app.use('/api/v1/achievements', achievementRouter)
+app.use('/api/v1/auth', authRouter)
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' })
