b382de6a22ce144446853d39a257ae9ff0e88fc7
fallowShades <fallowshades@gmail.com>
2023-10-08 12:52:02 +0200

    unauthenticated error
    https://hamoria.atlassian.net/browse/HM-36

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
 import { hashPassword } from '../utils/passwordUtils.js'
+import { UnauthenticatedError } from '../errors/customErrors.js'
 
 export const register = async (req, res) => {
   // first registered user is an admin
@@ -15,5 +16,11 @@ export const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: 'user created' })
 }
 export const login = async (req, res) => {
-  res.send('register')
+  // check if user exists
+  // check if password is correct
+
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) throw new UnauthenticatedError('invalid credentials')
+
+  res.send('login route')
 }
