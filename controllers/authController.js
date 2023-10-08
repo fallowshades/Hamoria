7b1f9510511bc4f16e32ab2efdf3e163c84bb1e7
fallowShades <fallowshades@gmail.com>
2023-10-08 12:45:58 +0200

    setup password utils
    https://hamoria.atlassian.net/browse/HM-36

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,18 @@
 import { StatusCodes } from 'http-status-codes'
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
+import { hashPassword } from '../utils/passwordUtils.js'
 
 export const register = async (req, res) => {
   // first registered user is an admin
   const isFirstAccount = (await User.countDocuments()) === 0
   req.body.role = isFirstAccount ? 'admin' : 'user'
 
-  // a random value that is added to the password before hashing
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const hashedPassword = await hashPassword(req.body.password)
   req.body.password = hashedPassword
 
   const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' })
 }
 export const login = async (req, res) => {
   res.send('register')
