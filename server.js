869b5c4f92bd8abdbd0162b69bc0b9716b155b34
fallowShades <fallowshades@gmail.com>
2023-10-09 19:21:23 +0200

    Cloudinary - setup instance
    https://hamoria.atlassian.net/browse/HM-57

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ import userRouter from './routes/userRouter.js'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import path from 'path'
+import cloudinary from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
 
 dotenv.config()
 const __dirname = dirname(fileURLToPath(import.meta.url))
