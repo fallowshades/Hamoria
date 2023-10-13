347d8bb611e849018f11747a4a126fa25b840814
fallowShades <fallowshades@gmail.com>
2023-10-13 13:29:43 +0200

    pageBtnContainer - render buttons
    https://hamoria.atlassian.net/browse/HM-63

--- hamoriaClient/src/components/PageBtnContainer.jsx
+++ hamoriaClient/src/components/PageBtnContainer.jsx
@@ -2,12 +2,28 @@ import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 import Wrapper from '../assets/wrappers/PageBtnContainer'
 import { useLocation, Link, useNavigate } from 'react-router-dom'
 import { useAllAchievementsContext } from '../pages/AllAchievements'
+//import { isButtonElement } from 'react-router-dom/dist/dom'
 
 const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllAchievementsContext()
 
-  return <div>PageBtnContainer</div>
+  const pages = Array.from({ length: numOfPages }, (_, index) => index + 1)
+
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        {pages.map((pageNumber) => (
+          <button
+            className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+            key={pageNumber}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+    </Wrapper>
+  )
 }
 export default PageBtnContainer
