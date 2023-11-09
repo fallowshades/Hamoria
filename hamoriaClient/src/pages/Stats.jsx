0fbe04f2235dead5350168952962f3cf6abed307
fallowShades <fallowshades@gmail.com>
2023-11-09 14:42:13 +0100

    get stats with react query
    https://hamoria.atlassian.net/browse/HM-86

--- hamoriaClient/src/pages/Stats.jsx
+++ hamoriaClient/src/pages/Stats.jsx
@@ -3,22 +3,25 @@ import customFetch from '../utils/customFetch'
 import { useLoaderData } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 
+const statsQuery = {
+  queryKey: ['stats'],
+  queryFn: async () => {
+    const response = await customFetch.get('/achievements/stats')
+    return response.data
+  },
+}
+
 export const loader = async () => {
   return null
 }
 
 const Stats = () => {
-  const response = useQuery({
-    queryKey: ['stats'],
-    queryFn: () => customFetch.get('/achievements/stats'),
-  })
-  console.log(response)
-  if (response.isLoading) {
-    return <h1>Loading...</h1>
-  }
-  return <h1>react query</h1>
+  const { isLoading, isError, data } = useQuery(statsQuery)
 
-  const { defaultStats, monthlyApplications } = useLoaderData()
+  if (isLoading) return <h4>Loading...</h4>
+  if (isError) return <h4>Error...</h4>
+
+  const { defaultStats, monthlyApplications } = data
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
