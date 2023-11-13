88093bbc933fae8f77b4efe28dd1abb37c7ff5c7
fallowShades <fallowshades@gmail.com>
2023-11-13 15:27:37 +0100

    Setup params
    https://hamoria.atlassian.net/browse/HM-101

--- hamoriaClient/src/components/Filters.jsx
+++ hamoriaClient/src/components/Filters.jsx
@@ -5,7 +5,9 @@ import FormRange from './FormRange'
 import FormCheckbox from './FormCheckbox'
 
 const Filters = () => {
-  const { meta } = useLoaderData()
+  const { meta, params } = useLoaderData()
+  const { search, company, category, shipping, order, price } = params
+
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -13,6 +15,7 @@ const Filters = () => {
         type="search"
         label="search product"
         name="search"
+        defaultValue={search}
         size="input-sm"
       />
       {/* CATEGORIES */}
@@ -21,6 +24,7 @@ const Filters = () => {
         label="select category"
         name="category"
         list={meta.categories}
+        defaultValue={category}
         size="select-sm"
       />
       {/* COMPANIES */}
@@ -29,6 +33,7 @@ const Filters = () => {
         label="select company"
         name="company"
         list={meta.companies}
+        defaultValue={company}
         size="select-sm"
       />
       {/* ORDER */}
@@ -37,12 +42,25 @@ const Filters = () => {
         label="sort by"
         name="order"
         list={['a-z', 'z-a', 'high', 'low']}
+        defaultValue={order}
         size="select-sm"
       />
-      {/* SHIPPING */}
-      ;<FormCheckbox label="free shipping" name="shipping" size="checkbox-sm" />
       {/* PRICE */}
-      ;<FormRange label="select price" name="price" size="range-sm" />
+      ;
+      <FormRange
+        label="select price"
+        name="price"
+        price={price}
+        size="range-sm"
+      />
+      {/* SHIPPING */}
+      ;
+      <FormCheckbox
+        label="free shipping"
+        name="shipping"
+        defaultValue={shipping}
+        size="checkbox-sm"
+      />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm ">
         search
--- hamoriaClient/src/pages/AllSigns.jsx
+++ hamoriaClient/src/pages/AllSigns.jsx
@@ -3,11 +3,14 @@ import { convenientFetch } from '../utils/corsFetch'
 
 const url = '/products'
 export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ])
   const response = await convenientFetch(url)
   console.log(response)
   const products = response.data.data
   const meta = response.data.meta
-  return { products, meta }
+  return { products, meta, params }
 }
 
 const AllSigns = () => {
