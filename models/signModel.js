4f7978001ec1ad7dd298e3c8dd3ea8e2d07bbd0d
fallowShades <fallowshades@gmail.com>
2023-11-13 20:31:09 +0100

    sign Model
    https://hamoria.atlassian.net/browse/HM-105

---
+++ models/signModel.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose'
+
+const SignSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide product name'],
+      maxlength: [100, 'Name can not be more than 100 characters'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      maxlengh: [1000, 'Description can not be more than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide company'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+    colors: {
+      type: [String],
+      default: ['#222'],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('sign', SignSchema)
