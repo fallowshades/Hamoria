ac866d98ea5de6c416b4a95d9a1163ebff626280
fallowShades <fallowshades@gmail.com>
2023-11-13 12:27:06 +0100

    hero carousel
    https://hamoria.atlassian.net/browse/HM-95

--- hamoriaClient/src/pages/FeatureHome.jsx
+++ hamoriaClient/src/pages/FeatureHome.jsx
@@ -1,4 +1,12 @@
+import Wrapper from '../assets/wrappers/FeatureHome'
+import { Hero } from '../components'
+
 const FeatureHome = () => {
-  return <div>FeatureHome</div>
+  return (
+    <Wrapper>
+      <Hero />
+      <div>FeatureHome</div>
+    </Wrapper>
+  )
 }
 export default FeatureHome
