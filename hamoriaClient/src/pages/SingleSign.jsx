edbcf83a115de3fba375b58e7053f4b74d17fdd2
fallowShades <fallowshades@gmail.com>
2023-11-14 17:29:04 +0100

    Add sign from singleSign page
    https://hamoria.atlassian.net/browse/HM-113

--- hamoriaClient/src/pages/SingleSign.jsx
+++ hamoriaClient/src/pages/SingleSign.jsx
@@ -3,6 +3,9 @@ import { formatPrice, convenientFetch } from '../utils/corsFetch'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
+import { useDispatch } from 'react-redux'
+import { addItem } from '../features/cart/cartSlice'
+
 export const loader = async ({ params }) => {
   const response = await convenientFetch(`/products/${params.id}`)
   return { product: response.data.data }
@@ -16,6 +19,19 @@ const SingleSign = () => {
   const [productColor, setProductColor] = useState(colors[0])
   const [amount, setAmount] = useState(1)
 
+  const dispatch = useDispatch()
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    amount,
+    productColor,
+    company,
+  }
+
+  const addToCart = () => {}
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value))
   }
@@ -91,10 +107,7 @@ const SingleSign = () => {
           </div>
           {/* CART BUTTON */}
           <div className="mt-10 ">
-            <button
-              className="btn btn-secondary btn-md"
-              onClick={() => console.log('add to bag')}
-            >
+            <button className="btn btn-secondary btn-md" onClick={addToCart}>
               Add to bag
             </button>
           </div>
