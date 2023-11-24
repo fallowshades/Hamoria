bba00ff7446d3b1403d8254c30a819ac4857c7bf
fallowShades <fallowshades@gmail.com>
2023-11-24 17:59:18 +0100

    basic orientation routing
    https://hamoria.atlassian.net/browse/HM-126

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ import reviewRouter from './routes/reviewRouter.js'
 import orderRouter from './routes/ordersRouter.js'
 
 import prefixRouter from './routes/prefixRouter.js'
+import orientationRouter from './routes/orientationRouter.js'
 dotenv.config()
 
 cloudinary.config({
@@ -48,6 +49,7 @@ app.use('/api/v1/signs', signRouter)
 app.use('/api/v1/reviews', reviewRouter)
 app.use('/api/v1/orders', orderRouter)
 app.use('/api/v1/prefixes', prefixRouter)
+app.use('/api/v1/orientations', orientationRouter)
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './hamoriaClient/dst', 'index.html'))
