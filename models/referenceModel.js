f2f9dec847d991f78dc7b1b6ed2eaaabf16c8de5
fallowShades <fallowshades@gmail.com>
2023-12-02 15:50:52 +0100

    reference populate
    https://hamoria.atlassian.net/browse/HM-147

--- models/referenceModel.js
+++ models/referenceModel.js
@@ -2,20 +2,19 @@ import mongoose from 'mongoose'
 import { TOUCH_TYPE, FACE_EXPRESSION } from '../utils/constants.js'
 
 const ReferenceSchema = new mongoose.Schema({
+  orderid: String,
   position: {
     type: String,
-    required: true,
   },
-  bodyContact: {
+  bodycontact: {
     type: String,
-    required: true,
   },
-  touchType: {
+  touchtype: {
     type: String,
     enum: [TOUCH_TYPE],
     default: TOUCH_TYPE.NULL,
   },
-  faceExpression: {
+  faceexpression: {
     type: String,
     enum: [FACE_EXPRESSION],
     default: FACE_EXPRESSION.NULL,
