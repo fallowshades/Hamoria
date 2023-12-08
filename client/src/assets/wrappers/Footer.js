29de98ccb9fa6ffe06f69efaa3307812cf5438b3
fallowShades <fallowshades@gmail.com>
2023-12-08 12:50:54 +0100

    Fix footer css
    https://hamoria.atlassian.net/browse/HM-151

--- client/src/assets/wrappers/Footer.js
+++ client/src/assets/wrappers/Footer.js
@@ -12,6 +12,18 @@ const Wrapper = styled.footer`
     justify-content: space-around;
     gap: 2em;
   }
+
+   display: flex;
+    align-items: center;
+    color: var(--text-secondary-color);
+    padding: 1rem 0;
+    padding-left: 2.5rem;
+    text-transform: capitalize;
+    transition: color 0.3s ease-in-out;
+  }
+  .nav-link:hover {
+    color: var(--primary-500);
+    transition: var(--transition);
 `
 
 export default Wrapper
