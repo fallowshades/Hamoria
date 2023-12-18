96673231755e63f60499ffc3a3b298cbc00387df
fallowShades <fallowshades@gmail.com>
2023-12-18 13:34:47 +0100

    render basic pagination container
    https://hamoria.atlassian.net/browse/HM-182

--- client/src/components/courses/handparts/HandButtonContainer.jsx
+++ client/src/components/courses/handparts/HandButtonContainer.jsx
@@ -1,4 +1,41 @@
+import Wrapper from '../../../assets/wrappers/handparts/HandButtonContainer'
+import { FaHandPointLeft, FaHandPointRight } from 'react-icons/fa6'
+import { useAllReferenceContext } from '../../../pages/handparts/AllReference'
+
 const HandButtonContainer = () => {
-  return <div>HandButtonContainer</div>
+  const {
+    data: { numOfPages, currentPage },
+  } = useAllReferenceContext()
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1
+  })
+  console.log(pages)
+  return (
+    <Wrapper>
+      <button className="btn prev-btn">
+        <FaHandPointLeft />
+        prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              className={`btn page-btn ${
+                pageNumber === currentPage && 'active'
+              }`}
+            >
+              {pageNumber}
+            </button>
+          )
+        })}
+      </div>
+      <button className="btn next-btn">
+        next
+        <FaHandPointRight />
+      </button>
+    </Wrapper>
+  )
 }
 export default HandButtonContainer
