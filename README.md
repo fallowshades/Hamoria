8ddfda961d2e105ad475c441d336a94c2dd16798
fallowShades <fallowshades@gmail.com>
2023-12-22 19:40:49 +0100

    get all orientations -server
    https://hamoria.atlassian.net/browse/HM-186

--- README.md
+++ README.md
@@ -207,7 +207,57 @@ export const validateIdParam = withValidationErrors([
 orientationController.jsx
 
 ```js
+export const getAllOrientations = async (req, res) => {
+  const {
+    orderid,
+    fingerdirection,
+    fingerdirection2,
+    palmdirection,
+    palmdirection2,
+    sort,
+  } = req.body
 
+  const queryObject = {}
+
+  if (fingerdirection && fingerdirection !== 'all') {
+    queryObject.fingerdirection = fingerdirection
+  }
+  if (fingerdirection2 && fingerdirection2 !== 'all') {
+    queryObject.fingerdirection2 = fingerdirection2
+  }
+  if (palmdirection && palmdirection !== 'all') {
+    queryObject.palmdirection = palmdirection
+  }
+  if (palmdirection2 && palmdirection2 !== 'all') {
+    queryObject.palmdirection2 = palmdirection2
+  }
+
+  const sortOptions = {
+    'a-z': 'fingerdirection',
+    'z-a': '-fingerdirection',
+  }
+
+  const sortKey = sortOptions[sort] || sortOptions['a-z']
+  // setup pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  const orienetation = await Orientation.find({})
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit)
+
+  const totalOrientations = await Orientation.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalOrientations / limit)
+
+  res.status(StatusCodes.OK).json({
+    totalOrientations,
+    numOfPages,
+    currentPage: page,
+    orientations: orienetation,
+  })
+}
 ```
 
 OrientationModel.jsx
--- controllers/orientationController.js
+++ controllers/orientationController.js
@@ -16,10 +16,58 @@ export const createOrientation = async (req, res) => {
 }
 
 export const getAllOrientations = async (req, res) => {
+  const {
+    orderid,
+    fingerdirection,
+    fingerdirection2,
+    palmdirection,
+    palmdirection2,
+    sort,
+  } = req.body
+
+  const queryObject = {}
+
+  if (fingerdirection && fingerdirection !== 'all') {
+    queryObject.fingerdirection = fingerdirection
+  }
+  if (fingerdirection2 && fingerdirection2 !== 'all') {
+    queryObject.fingerdirection2 = fingerdirection2
+  }
+  if (palmdirection && palmdirection !== 'all') {
+    queryObject.palmdirection = palmdirection
+  }
+  if (palmdirection2 && palmdirection2 !== 'all') {
+    queryObject.palmdirection2 = palmdirection2
+  }
+
+  const sortOptions = {
+    'a-z': 'fingerdirection',
+    'z-a': '-fingerdirection',
+  }
+
+  const sortKey = sortOptions[sort] || sortOptions['a-z']
+  // setup pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
   //const packagedData = await readLocalFile()
   const orienetation = await Orientation.find({})
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit)
 
-  res.status(StatusCodes.OK).json({ orientations: orienetation })
+  const totalOrientations = await Orientation.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalOrientations / limit)
+
+  res
+    .status(StatusCodes.OK)
+    .json({
+      totalOrientations,
+      numOfPages,
+      currentPage: page,
+      orientations: orienetation,
+    })
 }
 
 export const getSingleOrientation = async (req, res) => {
