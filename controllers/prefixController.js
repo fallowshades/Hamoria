96fabf9793b5b3da5dd408f050940d7c71477152
fallowShades <fallowshades@gmail.com>
2023-12-23 12:42:56 +0100

    remote prefix crud
    https://hamoria.atlassian.net/browse/HM-187

--- controllers/prefixController.js
+++ controllers/prefixController.js
@@ -1,15 +1,45 @@
 import { StatusCodes } from 'http-status-codes'
-import PrefixModel from '../models/prefixModel.js'
+import Prefix from '../models/prefixModel.js'
 import 'express-async-errors'
 
-export const createPrefix = async (req, res) => {}
-
-export const getAllPrefixes = async (req, res) => {
-  res.status(StatusCodes.OK).json({ prefixes: {} })
+export const createPrefix = async (req, res) => {
+  const { Connectionid, position, hand } = req.body
+  const prefix = await Prefix.create(req.body)
+  res.status(StatusCodes.OK).json({ prefix })
 }
 
-export const getSinglePrefix = async (req, res) => {}
+export const getAllPrefixes = async (req, res) => {
+  const prefix = await Prefix.find({})
+  res.status(StatusCodes.OK).json({ prefixes: prefix })
+}
 
-export const updatePrefix = async (req, res) => {}
+export const getSinglePrefix = async (req, res) => {
+  const { id } = req.params
+  const prefix = await Prefix.findById(id)
+  if (!prefix) {
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: 'no prefix with id' })
+  }
+  res.status(StatusCodes.OK).json({ prefix })
+}
 
-export const deletePrefix = async (req, res) => {}
+export const updatePrefix = async (req, res) => {
+  const { id } = req.params
+
+  const updatedPrefix = await Prefix.findByIdAndUpdate(id, req.body, {
+    new: true,
+  })
+
+  if (!updatedPrefix) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no reference with id ${id}` })
+  }
+  res.status(StatusCodes.OK).json({ updatedPrefix })
+}
+
+export const deletePrefix = async (req, res) => {
+  const { id } = req.params
+  const removedPrefix = await Prefix.findByIdAndDelete(id)
+
+  res.status(StatusCodes.OK).json({ removedPrefix })
+}
