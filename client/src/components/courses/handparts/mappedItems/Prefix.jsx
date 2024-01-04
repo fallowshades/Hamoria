e4ba572e8d0bbaea5da28a9abf4ac3fe3488e533
fallowShades <fallowshades@gmail.com>
2024-01-04 14:35:26 +0100

    prefix edit toggle
    https://hamoria.atlassian.net/browse/HM-188

--- client/src/components/courses/handparts/mappedItems/Prefix.jsx
+++ client/src/components/courses/handparts/mappedItems/Prefix.jsx
@@ -4,20 +4,37 @@ import SignInfo from './SignInfo'
 import { svgTeenBoyBody } from '../../../common'
 import { EditPrefix } from '../mappedItems'
 import { Form } from 'react-router-dom'
+import { useState } from 'react'
 
 const Prefix = ({ _id, Connectionid, position, hand }) => {
+  const [isEdit, setIsEdit] = useState(false)
   return (
     <Wrapper>
       <SectionTitle text={Connectionid} AddclassName="text-black" />
 
       <div className="content">
         <div className="content-center">
-          <SignInfo icon={svgTeenBoyBody} text={position} />
-          <SignInfo icon={svgTeenBoyBody} text={hand} />
+          {isEdit ? (
+            <EditPrefix _id={_id} />
+          ) : (
+            <>
+              {' '}
+              <SignInfo icon={svgTeenBoyBody} text={position} />
+              <SignInfo icon={svgTeenBoyBody} text={hand} />
+            </>
+          )}
         </div>
       </div>
       <footer className="actions">
-        <EditPrefix _id={_id} />
+        <button
+          className="btn edit-btn"
+          onClick={() => {
+            setIsEdit(!isEdit)
+          }}
+        >
+          {' '}
+          {isEdit ? 'prefix' : ' Edit'}
+        </button>
         <Form method="post" action={`../delete-prefix/${_id}`}>
           <button type="submit" className="btn delete-btn">
             Delete
