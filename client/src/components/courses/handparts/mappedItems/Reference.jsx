a1568dd6e0e5e1eef016c1ec612e46f59bbfd668
fallowShades <fallowshades@gmail.com>
2024-01-04 14:20:57 +0100

    reference edit toggle

--- client/src/components/courses/handparts/mappedItems/Reference.jsx
+++ client/src/components/courses/handparts/mappedItems/Reference.jsx
@@ -6,6 +6,8 @@ import { FaLocationArrow } from 'react-icons/fa'
 import { EditReference } from '../mappedItems'
 import { Form } from 'react-router-dom'
 
+import { useState } from 'react'
+
 const Reference = ({
   _id,
   position,
@@ -14,42 +16,57 @@ const Reference = ({
   faceexpression,
   link,
 }) => {
+  const [isEdit, setIsEdit] = useState(false)
   return (
     <Wrapper>
       <SectionTitle text={link} AddclassName="text-black" link />
 
       <div className="content">
         <div className="content-center">
-          <div>
-            <SignInfo
-              icon={<FaLocationArrow />}
-              text={'position: ' + (position ?? '')}
-            />
-            <SignInfo
-              icon={<FaLocationArrow />}
-              text={'face expression: ' + (faceexpression ?? '')}
-            />
-          </div>
-
-          <div>
-            <SignInfo
-              icon={<FaLocationArrow />}
-              text={'touchtype: ' + (touchtype ?? '')}
-            />
-            <SignInfo
-              icon={<FaLocationArrow />}
-              text={'bodyContact: ' + (bodycontact ?? '')}
-            />
-          </div>
-          <footer className="actions">
+          {isEdit ? (
             <EditReference _id={_id} />
-            <Form method="post" action={`../delete-reference/${_id}`}>
-              <button type="submit" className="btn delete-btn">
-                Delete
-              </button>
-            </Form>
-          </footer>
+          ) : (
+            <>
+              {' '}
+              <div>
+                <SignInfo
+                  icon={<FaLocationArrow />}
+                  text={'position: ' + (position ?? '')}
+                />
+                <SignInfo
+                  icon={<FaLocationArrow />}
+                  text={'face expression: ' + (faceexpression ?? '')}
+                />
+              </div>
+              <div>
+                <SignInfo
+                  icon={<FaLocationArrow />}
+                  text={'touchtype: ' + (touchtype ?? '')}
+                />
+                <SignInfo
+                  icon={<FaLocationArrow />}
+                  text={'bodyContact: ' + (bodycontact ?? '')}
+                />
+              </div>
+            </>
+          )}
         </div>
+        <footer className="actions">
+          <button
+            className="btn edit-btn"
+            onClick={() => {
+              setIsEdit(!isEdit)
+            }}
+          >
+            {isEdit ? 'reference' : ' Edit'}
+          </button>
+
+          <Form method="post" action={`../delete-reference/${_id}`}>
+            <button type="submit" className="btn delete-btn">
+              Delete
+            </button>
+          </Form>
+        </footer>
       </div>
     </Wrapper>
   )
